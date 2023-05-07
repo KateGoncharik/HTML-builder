a5346db5f1809da3dca76a80e2c2f9dbb1264c7e
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2023-05-07 18:13:23 +0300

    feat: make second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const file = 'text.txt';
+fs.access(file, fs.F_OK, (err) => {
+    if (err) {
+        fs.writeFile(
+            path.join(__dirname, file),
+            '',
+            (err) => {
+                if (err) throw err;
+            }
+        );
+    }
+})
+
+process.stdout.write('Write some text!');
+process.stdin.on('data', data => {
+    if (data.toString().trim() === 'exit') {
+        process.exit(1);
+    }
+    fs.appendFile(
+        path.join(__dirname, file),
+        data.toString(),
+        err => {
+            if (err) throw err;
+        }
+    )
+});
