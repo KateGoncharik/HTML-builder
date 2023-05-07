80a80eca659040afe7a21cec33e1ed04428c7d05
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2023-05-07 12:18:26 +0300

    feat: make first task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,2 +1,11 @@
 const fs = require('node:fs');
 const path = require('node:path');
+
+const stream = fs.createReadStream(
+    path.join(__dirname, 'text.txt'),
+    'utf-8',
+)
+stream.on('data', data => console.log(data));
+stream.on('error', error => {
+    console.log('Error', error.message);
+})
