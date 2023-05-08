d0a3aac328b970054a6859c1a10f64a48511e156
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2023-05-08 23:24:14 +0300

    feat: make forth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
 
+async function copyDir() {
+    const dirFrom = path.join(__dirname, 'files');
+    const dirTo = path.join(__dirname, 'files-copy');
+    await fsPromises.rm(dirTo, { recursive: true, force: true });
+    await fsPromises.mkdir(dirTo, { recursive: true });
+    const files = await fsPromises.readdir(dirFrom);
+    for (const file of files) {
+        const fileFrom = path.join(dirFrom, file);
+        const fileTo = path.join(dirTo, file);
+        await fsPromises.copyFile(fileFrom, fileTo);
+    }
+}
+
+copyDir();
